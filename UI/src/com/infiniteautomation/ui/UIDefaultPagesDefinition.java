e18bc0b07f0677768c7cddfa58401625b963f0b2
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-09-24 15:36:43 -0600

    Replace static method calls in ModuleElementDefinition with injected DAO

--- UI/src/com/infiniteautomation/ui/UIDefaultPagesDefinition.java
+++ UI/src/com/infiniteautomation/ui/UIDefaultPagesDefinition.java
@@ -7,6 +7,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.serotonin.m2m2.db.dao.SystemSettingsDao;
@@ -20,24 +21,27 @@ import com.serotonin.m2m2.vo.User;
  */
 public class UIDefaultPagesDefinition extends DefaultPagesDefinition {
 
+    @Autowired
+    protected SystemSettingsDao systemSettingsDao;
+
     @Override
     public String getLoginPageUri(HttpServletRequest request, HttpServletResponse response) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_LOGIN_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_LOGIN_PAGE);
     }
 
     @Override
     public String getLoginErrorUri(HttpServletRequest request, HttpServletResponse response) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_LOGIN_ERROR_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_LOGIN_ERROR_PAGE);
     }
 
     @Override
     public String getLogoutSuccessUri(HttpServletRequest request, HttpServletResponse response) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_LOGOUT_SUCCESS_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_LOGOUT_SUCCESS_PAGE);
     }
 
     @Override
     public String getPasswordResetPageUri() {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_PASSWORD_RESET_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_PASSWORD_RESET_PAGE);
     }
 
     @Override
@@ -47,27 +51,27 @@ public class UIDefaultPagesDefinition extends DefaultPagesDefinition {
 
     @Override
     public String getLoggedInPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_LOGGED_IN_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_LOGGED_IN_PAGE);
     }
 
     @Override
     public String getFirstUserLoginPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_FIRST_USER_LOGIN_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_FIRST_USER_LOGIN_PAGE);
     }
 
     @Override
     public String getLoggedInPageUriPreHome(HttpServletRequest request, HttpServletResponse response, User user) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_LOGGED_IN_PAGE_PRE_HOME);
+        return systemSettingsDao.getValue(UICommon.UI_LOGGED_IN_PAGE_PRE_HOME);
     }
 
     @Override
     public String getUnauthorizedPageUri(HttpServletRequest request, HttpServletResponse response, User user) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_UNAUTHORIZED_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_UNAUTHORIZED_PAGE);
     }
 
     @Override
     public String getNotFoundPageUri(HttpServletRequest request, HttpServletResponse response) {
-        String page = SystemSettingsDao.getInstance().getValue(UICommon.UI_NOT_FOUND_PAGE);
+        String page = systemSettingsDao.getValue(UICommon.UI_NOT_FOUND_PAGE);
 
         if (!StringUtils.isEmpty(page)) {
             if (request != null) {
@@ -82,11 +86,11 @@ public class UIDefaultPagesDefinition extends DefaultPagesDefinition {
 
     @Override
     public String getErrorPageUri(HttpServletRequest request, HttpServletResponse response) {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_ERROR_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_ERROR_PAGE);
     }
 
     @Override
     public String getEmailVerificationPageUri() {
-        return SystemSettingsDao.getInstance().getValue(UICommon.UI_EMAIL_VERIFICATION_PAGE);
+        return systemSettingsDao.getValue(UICommon.UI_EMAIL_VERIFICATION_PAGE);
     }
 }
