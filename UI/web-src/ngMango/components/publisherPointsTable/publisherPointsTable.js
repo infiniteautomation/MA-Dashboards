e616df9f8dadd3d0b42682495521527eca5b89b0
Pier Puccini <pierpuccini@users.noreply.github.com>
2021-11-17 19:39:37 -0500

    #RAD-1354 Succesfully deleting points

--- UI/web-src/ngMango/components/publisherPointsTable/publisherPointsTable.js
+++ UI/web-src/ngMango/components/publisherPointsTable/publisherPointsTable.js
@@ -13,17 +13,12 @@ const DEFAULT_COLUMNS = [
     // { name: 'enabled', label: 'common.enabled', selectedByDefault: true }
 ];
 
-const VALIDATION_MESSAGE_PROPERTY_MAP = {
-    purgeType: 'purgePeriod.type',
-    purgePeriod: 'purgePeriod.periods',
-    name: 'common.name'
-};
 class PublisherPointsTableController extends TableController {
     static get $inject() {
-        return ['$scope', '$element', '$injector', 'maDialogHelper'];
+        return ['$scope', '$element', '$injector'];
     }
 
-    constructor($scope, $element, $injector, maDialogHelper) {
+    constructor($scope, $element, $injector) {
         super({
             $scope,
             $element,
@@ -36,7 +31,6 @@ class PublisherPointsTableController extends TableController {
             selectMultiple: true
         });
 
-        this.DialogHelper = maDialogHelper;
         this.pointsToPublish = new Map();
     }
 
@@ -157,165 +151,6 @@ class PublisherPointsTableController extends TableController {
     pointsChanged() {
         // ma-data-point-selector is not part of the form as it is in a drop down dialog, have to manually set the form dirty
         this.form.$setDirty();
-    }
-
-    buildPointsToSave(publishedPoint) {
-        this.form.$setDirty();
-        if (publishedPoint.action === 'DELETE' && publishedPoint.isNew() && this.pointsToPublish.has(publishedPoint.dataPointXid)) {
-            this.pointsToPublish.delete(publishedPoint.dataPointXid);
-        } else {
-            this.pointsToPublish.set(publishedPoint.xid, publishedPoint);
-        }
-        console.log('point map single', this.pointsToPublish);
-    }
-
-    savePoints(event) {
-        this.validationMessages = [];
-        this.errorMessages = [];
-
-        const allPointsToPublish = [...this.pointsToPublish.values()];
-
-        const requests = allPointsToPublish.map((pPoint) => {
-            const request = {
-                xid: pPoint.getOriginalId() || pPoint.xid,
-                body: pPoint
-            };
-
-            if (pPoint.isNew()) {
-                request.action = 'CREATE';
-            } else {
-                request.action = pPoint.action || 'UPDATE';
-            }
-
-            return request;
-        });
-
-        console.log('requests', requests);
-
-        if (requests.length <= 0) return null;
-
-        this.bulkTask = new this.resourceService.Bulk({
-            action: null,
-            requests
-        });
-
-        return this.bulkTask
-            .start(this.$scope)
-            .then(
-                (resource) => {
-                    this.saveMultipleComplete(resource, allPointsToPublish);
-                },
-                (error) => {
-                    this.notifyBulkEditError(error);
-                },
-                (resource) => {
-                    // progress
-                }
-            )
-            .finally(() => {
-                delete this.bulkTask;
-            });
-    }
-
-    saveMultipleComplete(resource, savedPoints) {
-        const { hasError } = resource.result;
-        const { responses } = resource.result;
-
-        responses.forEach((response, i) => {
-            const point = savedPoints[i];
-            if (response.body && ['CREATE', 'UPDATE'].includes(response.action)) {
-                angular.copy(response.body, point);
-            }
-        });
-
-        if (hasError) {
-            const validationMessages = [];
-
-            responses.forEach((response, i) => {
-                const message = response.error && response.error.localizedMessage;
-                if (message && !this.errorMessages.includes(message)) {
-                    this.errorMessages.push(message);
-                }
-
-                if (response.httpStatus === 422) {
-                    const { messages } = response.error.result;
-                    messages.forEach((m) => {
-                        const validationMessage = `${m.level}: ${response.xid} - ${m.message} ${m.property}`;
-                        if (!this.errorMessages.includes(validationMessage)) {
-                            this.errorMessages.push(validationMessage);
-                        }
-
-                        const found = validationMessages.find((m2) => m.level === m2.level && m.property === m2.property && m.message === m2.message);
-
-                        if (!found) {
-                            validationMessages.push({ ...m, xid: response.xid });
-                        }
-                    });
-                }
-            });
-            this.validationMessages = this.fixValidationMessages(validationMessages);
-        } else {
-            this.pointsToPublish = new Map();
-            this.setViewValue(savedPoints);
-            this.render();
-        }
-
-        this.notifyBulkEditComplete(resource);
-    }
-
-    notifyBulkEditComplete(resource) {
-        const numErrors = resource.result.responses.reduce((accum, response) => (response.error ? accum + 1 : accum), 0);
-
-        const toastOptions = {
-            textTr: [null, resource.position, resource.maximum, numErrors],
-            hideDelay: 10000,
-            classes: 'md-warn'
-        };
-
-        switch (resource.status) {
-            case 'CANCELLED':
-                toastOptions.textTr[0] = 'ui.app.bulkEditCancelled';
-                break;
-            case 'TIMED_OUT':
-                toastOptions.textTr[0] = 'ui.app.bulkEditTimedOut';
-                break;
-            case 'ERROR':
-                toastOptions.textTr[0] = 'ui.app.bulkEditError';
-                toastOptions.textTr.push(resource.error.localizedMessage);
-                break;
-            case 'SUCCESS':
-                if (!numErrors) {
-                    toastOptions.textTr = ['ui.app.bulkEditSuccess', resource.position];
-                    delete toastOptions.classes;
-                    this.errorMessages = [];
-                    this.validationMessages = [];
-                } else {
-                    toastOptions.textTr[0] = 'ui.app.bulkEditSuccessWithErrors';
-                }
-                break;
-            default:
-                break;
-        }
-
-        this.DialogHelper.toastOptions(toastOptions);
-    }
-
-    notifyBulkEditError(error) {
-        this.DialogHelper.toastOptions({
-            textTr: ['ui.app.errorStartingBulkEdit', error.mangoStatusText],
-            hideDelay: 10000,
-            classes: 'md-warn'
-        });
-    }
-
-    fixValidationMessages(validationMessages) {
-        validationMessages.forEach((vm) => {
-            const newKey = VALIDATION_MESSAGE_PROPERTY_MAP[vm.property];
-            if (newKey) {
-                vm.property = newKey;
-            }
-        });
-        return validationMessages;
     }
 }
 
