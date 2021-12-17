9ccd42ba2daf324da9c372a14edd10ad696bdda2
Jared Wiltshire <jazdw@users.noreply.github.com>
2021-12-17 09:50:44 -0700

    Update math.js, papaparse, xlsx

--- UI/web-src/ngMango/services/math.js
+++ UI/web-src/ngMango/services/math.js
@@ -2,25 +2,23 @@
  * Copyright (C) 2021 Radix IoT LLC. All rights reserved.
  */
 
-import math from 'mathjs';
-
+import {create, all} from 'mathjs';
 
 MathFactory.$inject = [];
 function MathFactory() {
-    const restricted = math.create();
 
-    restricted.import({
+    const math = create(all);
+
+    math.import({
         import: function () { throw new Error('Function import is disabled'); },
         createUnit: function () { throw new Error('Function createUnit is disabled'); },
-        eval: function () { throw new Error('Function eval is disabled'); },
+        evaluate: function () { throw new Error('Function evaluate is disabled'); },
         parse: function () { throw new Error('Function parse is disabled'); },
         simplify: function () { throw new Error('Function simplify is disabled'); },
         derivative: function () { throw new Error('Function derivative is disabled'); }
     }, {override: true});
-    
-    return restricted;
+
+    return math;
 }
 
 export default MathFactory;
-
-
